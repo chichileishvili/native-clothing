98cc7d8fdbbea8674b8255698fe8745bceda20fa
chichileishvili <79799309+chichileishvili@users.noreply.github.com>
2023-10-15 22:53:31 +0400

    name correction

--- src/components/directory/directory.component.tsx
+++ src/components/directory/directory.component.tsx
@@ -23,13 +23,13 @@ const categories = [
   },
   {
     id: 4,
-    title: 'woman',
+    title: 'women',
     imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
     route: 'shop/womens',
   },
   {
     id: 5,
-    title: 'man',
+    title: 'men',
     imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
     route: 'shop/mens',
   },
--- src/shop-data.js
+++ src/shop-data.js
@@ -147,7 +147,7 @@ const SHOP_DATA = [
     ],
   },
   {
-    title: 'Woman',
+    title: 'Women',
     items: [
       {
         id: 23,
@@ -194,7 +194,7 @@ const SHOP_DATA = [
     ],
   },
   {
-    title: 'Man',
+    title: 'Men',
     items: [
       {
         id: 30,
